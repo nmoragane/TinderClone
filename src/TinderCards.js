c3b638329fb1ae51120668df2ae078822ef7065e
Nayomal Lakshitha <nmoragane@gmail.com>
2020-12-22 12:23:26 +0530

    Added header

---
+++ src/TinderCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/TinderCards.css'
+
+function TinderCards() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default TinderCards;
